b705fe7cd4bf54ec0ce5aaa352d0a1d32bce6969
Om Jaiswal <jom15718@gmail.com>
2026-02-05 17:49:25 +0530

    Add additional commented code examples for Promises, Callbacks, and Async/Await in async_js_1.js

--- Async_JS/async_js_1.js
+++ Async_JS/async_js_1.js
@@ -46,4 +46,60 @@
 // .then((raw) => console.log(raw.data));
 
 // Promise
+// let parchi = new Promise(function (resolve, rejected) {
+//   fetch(`https://randomuser.me/api/`)
+//     .then((raw) => raw.json())
+//     .then((result) => {
+//       if (result.results[0].gender === "male") resolve();
+//       else rejected();
+//     });
+// });
+// parchi.then(function(){
+//     console.log("Green Button has pushed ")
+// })
+// .catch(function(){
+//     console.log("Red Button has pushed ")
 
+// });
+
+//Callback
+/* function abcd(a,b){
+}
+abcd(1, function (){}) */
+/* function abcd(a,b){
+    b()
+}
+abcd(1, function (){ console.log("call back hua hai ")}) */
+
+// use of call back
+/* function doSome(url,callback){
+    fetch(`url`)
+    .then(raw =>raw.json())
+    .then(result =>{
+        callback()
+    })
+}
+doSome("some url", function(){
+
+}) */
+//User a data mago jab data ajye to us data ke name gender and email ko print karo
+/* function getData(url, callback) {
+  fetch(url)
+    .then((raw) => raw.json())
+    .then((result) => {
+      callback(result);
+    });
+}
+getData("https://randomuser.me/api/", function (result) {
+  console.log(
+    result.results[0].gender,
+    result.results[0].email,
+    result.results[0].name.first,
+  );
+});
+ */
+
+
+
+
+// Async / await
