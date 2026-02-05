3a4f4e7e7b2179b3501fa6fd62b1315251577218
Om Jaiswal <jom15718@gmail.com>
2026-02-05 14:16:49 +0530

    Initial commit - upload all folders

---
+++ Async_JS/async_js_1.js
@@ -0,0 +1,9 @@
+//SetTimeOut
+// setTimeout(function () {
+//     console.log("Om Jaiswal")
+// }, 5000);
+
+//Set Interval Function 
+// setInterval(function(){
+//     console.log("Yashu Jaiswal")
+// },1000)
