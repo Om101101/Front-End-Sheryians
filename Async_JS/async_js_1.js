dd026a2fe8f77e2394689e97c5a630d1dad98432
Om Jaiswal <jom15718@gmail.com>
2026-02-05 16:47:58 +0530

    Clean up commented code in async_js_1.js and update script tag in async_js_1.html

--- Async_JS/async_js_1.js
+++ Async_JS/async_js_1.js
@@ -3,23 +3,47 @@
 //     console.log("Om Jaiswal")
 // }, 5000);
 
-//Set Interval Function 
+//Set Interval Function
 // setInterval(function(){
 //     console.log("Yashu Jaiswal")
 // },1000)
 
-// If you want to stop the set interval Function 
+// If you want to stop the set interval Function
 // let st= setInterval(function(){
 //     console.log("Om Jaiswal ");
 // },1000)
+
 // clearInterval(st)
 
+// var count = 1;
+// const st = setInterval(function () {
+//   console.log(count);
+//   count++;
 
-var count =1;
-const st = setInterval(function(){
-    count++;
-    console.log("Om ")
-    if(count===10) clearInterval(st)
-},1000)
+//   if (count > 10) {
+//     clearInterval(st);
+//   }
+// }, 1000);
 
 //Fetch API
+//Random user api search in google
+// https://randomuser.me/api/ put in search bar
+
+// fetch(`https://randomuser.me/api/`)
+// .then(raw=> console.log(raw))
+
+// fetch(`https://randomuser.me/api/`)
+// .then(raw=> raw.json())
+// .then(readlbledata=> console.log(readlbledata))
+
+// fetch(`https://randomuser.me/api/`)
+// .then(raw=> raw.json())
+// .then(readlbledata=> console.log(readlbledata.results[0].gender))
+
+//Axios
+// <script src="https://unpkg.com/axios@1.13.2/dist/axios.min.js"></script>
+// axios.get(`https://randomuser.me/api/`)
+// .then((raw) => console.log(raw.data));
+
+// Promise
+
