430fcfd0def9773e60ee514f9069fbb0a645d564
Om Jaiswal <jom15718@gmail.com>
2026-02-05 14:30:10 +0530

    Refactor async_js_1.js to clean up commented code and maintain functionality

--- Async_JS/async_js_1.js
+++ Async_JS/async_js_1.js
@@ -7,3 +7,19 @@
 // setInterval(function(){
 //     console.log("Yashu Jaiswal")
 // },1000)
+
+// If you want to stop the set interval Function 
+// let st= setInterval(function(){
+//     console.log("Om Jaiswal ");
+// },1000)
+// clearInterval(st)
+
+
+var count =1;
+const st = setInterval(function(){
+    count++;
+    console.log("Om ")
+    if(count===10) clearInterval(st)
+},1000)
+
+//Fetch API
